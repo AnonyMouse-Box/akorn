a3522f0d59f20af675d3caa99b29cbd9f1f2f471
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2019-01-31 18:02:17 +0000

    Update starter.asm
    added comments until I know what everything does without looking it up

--- X86/starter.asm
+++ X86/starter.asm
@@ -1,20 +1,21 @@
 ;;starter.asm
 bits 32
 section .text
+  ;multiboot spec
   align 4
-  dd 0x1BADB002
-  dd 0x00
-  dd - (0x1BADB002 + 0x00)
+  dd 0x1BADB002             ;magic
+  dd 0x00                   ;flags
+  dd - (0x1BADB002 + 0x00)  ;checksum m+f+c must equal zero
 
 global start
-extern core
+extern core                 ;function defined in external code
 
 start:
-  cli
-  mov esp, stack_space
+  cli                       ;blocks interrupts
+  mov esp, stack_space      ;set stack pointer
   call core
-  hlt
+  hlt                       ;halts the cpu
 
 section .bss
-resp 8192
+resp 8192                   ;8KB for stack
 stack_space:
